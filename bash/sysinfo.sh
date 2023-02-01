42dbc70ece8b34a81f5f83bda1dfb99f7a86697d
jung078 <122487086+jung078@users.noreply.github.com>
2023-02-01 13:20:28 -0500

    Delete sysinfo.sh

--- bash/sysinfo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "FQDN:$(hostname)"       #it will display the hostname of the system
-echo "host information" 
-hostnamectl                   #to provide additional information of the host
-echo "IP Addresses:" 
-hostname -I                   #to display the network address (IP address) of the host
-df -h /                       #displays information about the disk space usage of the file systems that  contain root directory
