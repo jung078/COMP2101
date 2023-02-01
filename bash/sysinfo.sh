d63056b03fcaf7254415f0c287e9bb7debca8df6
jung078 <122487086+jung078@users.noreply.github.com>
2023-02-01 13:16:29 -0500

    Update sysinfo.sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "FQDN:$(hostname)"
-echo "host information"
-hostnamectl #vjhjhjgkhl
-echo "IP Addresses:"
-hostname -I
-df -h /
+echo "FQDN:$(hostname)"       #it will display the hostname of the system
+echo "host information" 
+hostnamectl                   #to provide additional information of the host
+echo "IP Addresses:" 
+hostname -I                   #to display the network address (IP address) of the host
+df -h /                       #displays information about the disk space usage of the file systems that  contain root directory
