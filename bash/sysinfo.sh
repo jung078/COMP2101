d8505f484efb51e1f02446c17a05121ad3c34599
jung078 <122487086+jung078@users.noreply.github.com>
2023-02-15 19:12:52 -0500

    Bash_Lab 2

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 Hostname=$(hostname)               #it will display hostname of a system
+FQDN_Hostname=$(hostname -f)
 Operating_System_Name_and_Version=$(hostnamectl | grep -w Operating | awk '{print$3$4$5}') #(hostnamecctl) provides additional information of the host, (grep -w) command searches for the word that matches in a given set of files
 IP_Address=$(ip route | grep default | awk '{print$3}')        #(ip route) command is used to find ip address used by computer
 Root_FreeSpace=$(df -h / | awk 'NR==2 {print $4}') #(df -h) displays information about the disk space usage of the file systems that contain root directory in human readable format
@@ -9,7 +10,7 @@ cat << EOF
 
 Report for my $Hostname
 ===================
-FQDN: $Hostname
+FQDN:$FQDN_Hostname
 Operating System Name and Version:$Operating_System_Name_and_Version
 IP Address:$IP_Address
 Root FileSystem Free Space:$Root_FreeSpace
